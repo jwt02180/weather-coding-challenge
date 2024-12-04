ceae7dcb0de06579f4767c5815958b18cac40444
Jameson Taylor <jameson.w.taylor@gmail.com>
2024-12-03 21:26:34 -0500

    configure image urls for NextJS;

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const weatherIcons = new URL(process.env.OPENWEATHER_ICON_BASE_URL);
+
 const nextConfig: NextConfig = {
-  /* config options here */
+	images: {
+		remotePatterns: [{
+			protocol: 'https',
+			hostname: weatherIcons.hostname,
+			port: '',
+			pathname: `${weatherIcons.pathname}**`
+		}],
+	}
 };
 
 export default nextConfig;
